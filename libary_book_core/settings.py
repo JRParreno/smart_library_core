14b2114732a1e0e3c26debf6d9379c4150bbb401
JRParreno <jhonrhayparreno22@gmailc.om>
2024-02-29 00:33:09 +0800

    handle debug database settings

--- libary_book_core/settings.py
+++ libary_book_core/settings.py
@@ -91,16 +91,18 @@ WSGI_APPLICATION = 'libary_book_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if not DEBUG:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
